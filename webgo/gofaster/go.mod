6bc8e1ab019ab2fd6eaaf7a4b97ca173c6eb60f8
ccfuncy <1376190473@qq.com>
2023-07-30 18:04:07 +0800

    7/30 实现限流中间件

--- webgo/gofaster/go.mod
+++ webgo/gofaster/go.mod
@@ -25,6 +25,7 @@ require (
 	golang.org/x/net v0.8.0 // indirect
 	golang.org/x/sys v0.6.0 // indirect
 	golang.org/x/text v0.8.0 // indirect
+	golang.org/x/time v0.3.0 // indirect
 	google.golang.org/genproto v0.0.0-20210602131652-f16073e35f0c // indirect
 	google.golang.org/grpc v1.48.0 // indirect
 	google.golang.org/protobuf v1.27.1 // indirect
--- webgo/gofaster/limit.go
+++ webgo/gofaster/limit.go
@@ -1,3 +1,27 @@
 package gofaster
 
-//限流中间件
+import (
+	"context"
+	"golang.org/x/time/rate"
+	"net/http"
+	"time"
+)
+
+// 限流中间件
+func Limit(limit, cap int) MiddlewareFunc {
+	limiter := rate.NewLimiter(rate.Limit(limit), cap)
+	return func(next HandlerFunc) HandlerFunc {
+		return func(ctx *Context) {
+			//实现限流
+			con, cancelFunc := context.WithTimeout(context.Background(), time.Duration(1)*time.Second)
+			defer cancelFunc()
+			err := limiter.WaitN(con, 1)
+			if err != nil {
+				ctx.String(http.StatusForbidden, "限流了")
+				return
+			}
+			next(ctx)
+
+		}
+	}
+}
